d5f1c331943afb2077b908766055670cc774d6a2
Careem <careemdev7@gmail.com>
2025-04-19 19:03:35 +0530

    dynamic variable

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Ex1=KE
+Ex2=RE
+
+echo "$Ex1 :  Hey! $Ex2 How are ypu?"
+echo "$Ex2 : I am good $Ex2. How are you doing?"
+echo "$Ex1 : Not bad. How is the course running?"
+echo "$Ex2 : All OK."
